a13b32ed82981aee379b13faf12176f1e4245b68
Luis Felipe Evilla <luisevilla588@gmail.com>
2022-05-29 00:53:47 -0500

    signin functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { useState } from 'react';
 
 //partials
 import Header from './partials/header/header'
 
+// screens
+import SigninScreen from './pages/signin.jsx';
+
+// components
 function App() {
+  const [ token, setToken ] = useState(localStorage.getItem('token'));
+
   return (
     <BrowserRouter>
-      <Header/>
-      <Routes>
-        <Route path="/" exact={true} element={<div className="App"></div>}></Route>
-      </Routes>
+      <div className='grid-contaier'>
+        <Header />
+        <main className='main'>
+          <div className='content'>
+            <Routes>
+              <Route path="/" exact={true} element={<div className="App"></div>}></Route>
+              <Route path='/signin' setToken={setToken} exact={true} element={token ? <div></div> : <SigninScreen/>}>
+                
+              </Route>
+            </Routes>
+          </div>
+        </main>
+      </div>
     </BrowserRouter>
   );
 }
---
+++ src/pages/signin.jsx
@@ -0,0 +1,71 @@
+import React, {useState, useEffect} from "react";
+import { Link, useNavigate} from "react-router-dom"
+import PropTypes from 'prop-types';
+import axios from "axios";
+
+import './signin.css'
+
+function SigninScreen(props) {
+    const [ token, setToken ] = useState(localStorage.getItem('token'));
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (token) {
+            navigate('/')
+        }
+    }, [token])
+
+    const handleSignin = async () => {
+        const username = document.getElementById('name').value;
+        const password = document.getElementById('password').value;
+
+        await axios.post('http://localhost:4000/signin', {
+            "username": username,
+            "password": password
+        }, {
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            }
+        }).then((result) => {
+            if (result.status === 200) {
+                localStorage.setItem('token', JSON.stringify(result.data.token))
+                setToken(result.data.token)
+            }
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    return (
+        <div className="container signin-container">
+            <div className="form-container">
+                <h2 className="row form-title">Ingresar</h2>
+                <div className="row">
+                    <div className="form-group">
+                        <label for="name" className="form-label">Nombre</label>
+                        <input className="form-control" id="name" type="text" placeholder="Ingresa tu nombre"></input>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="form-group">
+                        <label for="password" className="form-label">Contraseña</label>
+                        <input className="form-control" id="password" type="password"></input>
+                    </div>
+                </div>
+
+                <div className="submit-container">
+                    <button className="btn btn-success" type="submit" onClick={handleSignin}>Enviar</button>
+                    <div className="signup-link-container">
+                        <Link to="/signup">¿Áun no tienes una cuenta?</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+SigninScreen.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
+export default SigninScreen
